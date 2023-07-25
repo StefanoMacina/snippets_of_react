6205a7aa0b1553fc64cbd7333594b568d7945a0b
Stefano <maclion@live.com>
2023-07-25 18:08:38 +0200

    add : responsive

--- todoList/src/components/AddTodo.jsx
+++ todoList/src/components/AddTodo.jsx
@@ -1,27 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useGlobalContext } from "../context/context";
 
 const AddTodo = () => {
   const { addTodo } = useGlobalContext();
   const [newTodo, setNewTodo] = useState("");
+  const inputFocus = useRef()
 
   const handleChange = (e) => {
     setNewTodo(e.target.value);
   };
 
+  useEffect(() => {
+    inputFocus.current.focus()
+  })
+
   return (
     <section className="bg-slate-200 p-4 ">
-      <form className="ring-1 ring-orange-500 rounded-lg  grid grid-cols-1 shadow-lg lg:w-1/2 lg:mx-auto ">
-        <div className="flex flex-col justify-center items-center gap-4 p-4">
+      <form className=" rounded-lg  grid grid-cols-1  lg:w-1/2 lg:mx-auto ">
+        <div className="flex flex-col justify-center items-center gap-4 p-4 ">
           <label htmlFor="" className="font-semibold text-slate-800">
             Type something to do here
           </label>
           <input
+          ref={inputFocus}
             onChange={handleChange}
             value={newTodo}
             type="text"
             placeholder="  type here"
-            className="rounded-md"
+            className="rounded-md p-2 focus:outline-blue-500 "
           />
           <button
             onClick={(e) => {
